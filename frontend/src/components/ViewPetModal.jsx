6f608f23893d29e4c1d592ef967dc3a734789df1
Don Dioscoro Jones <don.platan@talentamp.com>
2023-11-06 20:50:20 +0800

    added missing files

---
+++ frontend/src/components/ViewPetModal.jsx
@@ -0,0 +1,129 @@
+import DefaultCat from "../assets/images/defaults/default-cat.jpg";
+import DefaultBird from "../assets/images/defaults/default-bird.jpg";
+import DefaultDog from "../assets/images/defaults/default-dog.jpg";
+import DefaultQuestionMark from "../assets/images/defaults/default-questionmark.jpg";
+import { Button, Modal, Image, Row, Col } from "react-bootstrap";
+import { toast } from "react-toastify";
+
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import axios from "axios";
+import { useEffect } from "react";
+
+const ViewPetModal = (props) => {
+  const { userInfo } = useSelector((state) => state.auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (userInfo.user.userType !== "admin") {
+      navigate("/");
+    }
+  }, []);
+
+  const { data, onHide } = props;
+
+  if (!data) {
+    return null; // or some other fallback UI
+  }
+
+  const handleDelete = () => {
+    console.log(data._id);
+    if (window.confirm(`Are you sure you want to delete ${data.name}?`)) {
+      deletePet(data._id);
+      onHide();
+    }
+  };
+
+  //! delete pet
+  const deletePet = async (petId) => {
+    try {
+      const petUrl = `http://localhost:3001/api/pet/`;
+      const params = {
+        id: petId,
+      };
+      const headers = {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      };
+      await axios
+        .delete(petUrl, { params }, { headers })
+        .then((response) => {
+          console.log(response.data);
+        })
+        .catch((error) => {
+          if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+            console.log(error.response.headers);
+          } else if (error.request) {
+            console.log(error.request);
+          } else {
+            console.log("Error", error.message);
+          }
+          console.log(error.config);
+        });
+    } catch (err) {
+      toast.error(err?.data?.message || err.error);
+    }
+  };
+
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-center">Pet Details</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Row>
+          <Col className="d-flex justify-content-center">
+            {data.species === "Cat" ? (
+              <Image variant="top" src={DefaultCat} height={250} width={250} />
+            ) : data.species === "Dog" ? (
+              <Image variant="top" src={DefaultDog} height={250} width={250} />
+            ) : data.species === "Bird" ? (
+              <Image variant="top" src={DefaultBird} height={250} width={250} />
+            ) : (
+              <Image src={DefaultQuestionMark} height={250} width={250} />
+            )}
+          </Col>
+          <Col>
+            <h4 className="fw-bold">{data.name}</h4>
+            <h6 className="mb-2 text-muted">{data.species}</h6>
+            <hr />
+            <p className="h6 fw-bold">
+              Age: <span className="text-muted">{data.age}</span>
+            </p>
+            <p className="h6 fw-bold">
+              Gender: <span className="text-muted">{data.gender}</span>
+            </p>
+            <p className="h6 fw-bold">
+              Breed: <span className="text-muted">{data.breed}</span>
+            </p>
+            <p className="h6 fw-bold">
+              For Adoption:{" "}
+              <span className="text-muted">
+                {data.isAdopted ? "No" : "Yes"}
+              </span>
+            </p>
+            <p className="h6 fw-bold">
+              Description: <br />{" "}
+              <span className="text-muted">{data.description}</span>
+            </p>
+          </Col>
+        </Row>
+      </Modal.Body>
+      <Modal.Footer className="d-flex justify-content-between">
+        <Button onClick={handleDelete} variant="danger">
+          Delete
+        </Button>
+        <Button onClick={onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default ViewPetModal;
