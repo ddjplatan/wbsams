6dbdf4156b14d925d1347140e833aa6ee1ba5d24
John Rey <john.cagaanan@supportzebra.com>
2023-12-04 21:46:14 +0800

    Added new controllers for spay/neuter appointments

--- backend/controllers/registrationController.js
+++ backend/controllers/registrationController.js
@@ -17,7 +17,7 @@ const registerToSpayAndNeuter = async () => {
       });
       res.status(200).setHeader("Content-Type", "application/json").json({
         success: true,
-        message: "Successfully applied for spay/neuters",
+        message: "Successfully applied for spay/neuter",
       });
     }
   } catch (error) {
@@ -25,15 +25,60 @@ const registerToSpayAndNeuter = async () => {
   }
 };
 
-// const confirmRegistration = async () => {
-//   try {
-//     const { appointmentId } = req.params;
-//     const spayNeuterAppointment = await SpayNeuterAppointment.findById(appointmentId)
-//     const spayNeuterInstanceId = spayNeuterAppointment.spayNeuterInstanceId
-//   }
-//   } catch (error) {}
-// };
+const confirmRegistration = async (req, res) => {
+  try {
+    const { appointmentId } = req.params;
+    const spayNeuterAppointment = await SpayNeuterAppointment.findById(
+      appointmentId
+    );
+
+    await SpayNeuterAppointment.findOneAndUpdate(
+      { _id: appointmentId },
+      { $set: { isApproved: true } },
+      { new: true }
+    );
+
+    const spayAndNeuterInstance = await SpayAndNeuter.findById(
+      spayNeuterAppointment.spayNeuterInstanceId
+    );
+
+    if (
+      spayAndNeuterInstance.registeredPets.length >= spayAndNeuterInstance.slots
+    ) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Slots are full" });
+    } else {
+      spayAndNeuterInstance.registeredPets.push(spayNeuterAppointment.pet);
+      await spayAndNeuterInstance.save();
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Successfully reserved slot for spay/neuter",
+    });
+  } catch (error) {
+    console.error(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Error confirming registration" });
+  }
+};
+
+const deleteRegistration = async (req, res, next) => {
+  try {
+    await SpayNeuterAppointment.deleteOne({ _id: req.params.appointmentId });
+    res
+      .status(200)
+      .setHeader("Content-Type", "application/json")
+      .json({ success: true, message: "Successfully deleted one appointment" });
+  } catch (err) {
+    res.status(500).json({ message: "Server error" });
+  }
+};
 
 module.exports = {
   registerToSpayAndNeuter,
+  confirmRegistration,
+  deleteRegistration,
 };
--- backend/routes/SpayNeuterAppointment.js
+++ backend/routes/SpayNeuterAppointment.js
@@ -2,3 +2,10 @@ const express = require("express");
 const router = express.Router();
 const protectedRoute = require("../middlewares/auth");
 const reqReceived = require("../middlewares/reqReceived");
+
+const {
+  adminValidator,
+  staffValidator,
+} = require("../middlewares/utils/validators");
+
+router.route;
