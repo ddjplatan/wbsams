7da1c9cc0a4a013047c2ae1917c3965454159509
John Rey <john.cagaanan@supportzebra.com>
2023-12-14 10:47:46 +0800

    Added news route

---
+++ backend/routes/News.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const protectedRoute = require("../middlewares/auth");
+const reqReceived = require("../middlewares/reqReceived");
+const multer = require("multer");
+const path = require("path");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./backend/public/uploads");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1024 * 1024 * 5 },
+  // dest: "./public/uploads",
+});
+
+const {
+  adminValidator,
+  staffValidator,
+} = require("../middlewares/utils/validators");
+
+const {
+  postNews,
+  getNewss,
+  getNews,
+  updateNews,
+  deleteNews,
+  deleteNewss,
+} = require("../controllers/newsController");
+
+router
+  .route("/")
+  .post(
+    reqReceived,
+    protectedRoute,
+    staffValidator,
+    upload.single("img"),
+    postNews
+  )
+  .get(reqReceived, getNewss)
+  .delete(reqReceived, protectedRoute, adminValidator, deleteNewss);
+
+router
+  .route("/:newsId")
+  .get(reqReceived, getNews)
+  .put(reqReceived, protectedRoute, staffValidator, updateNews)
+  .delete(reqReceived, protectedRoute, staffValidator, deleteNews);
+
+module.exports = router;
